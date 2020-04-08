520b46ad0c47686b35850bf585f4525b83edd4ee
paulo-santana2 <63335876+paulo-santana2@users.noreply.github.com>
2020-04-08 05:34:31 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ export default function Index() {
     <div>
       <p>Hello Paulo Santana.js</p>
       <p>The things we do for automatization...</p>
+      <p><b>That's the last time I'm editing this file!</b></p>
     </div>
   );
 }
